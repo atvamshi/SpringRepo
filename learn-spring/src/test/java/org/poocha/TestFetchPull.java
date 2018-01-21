9c1a80c57b3db96115b603d051c879a25ff98521
vthalapu <vamsi.thalapulapalli@optum.com>
2018-01-21 12:58:22 -0600

    Adding a file to test fetch and pull

---
+++ learn-spring/src/test/java/org/poocha/TestFetchPull.java
@@ -0,0 +1,18 @@
+package org.poocha;
+
+/**
+ * Project: org.learnspring.parent
+ * Package: org.poocha
+ * <p>
+ * User: vthalapu
+ * Date: 1/21/18
+ * Time: 12:57 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestFetchPull {
+    public static void main(String... args) {
+        System.out.println("Hello joomi");
+    }
+}

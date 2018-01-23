e9385555c01b04b2f64b539b50405f029ffb054a
vthalapu <vamsi.thalapulapalli@optum.com>
2018-01-22 18:43:50 -0600

    Added an app and app.properties

--- learn-spring/src/main/java/org/spring/learn/App.java
+++ learn-spring/src/main/java/org/spring/learn/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Project: learn-spring
@@ -21,6 +22,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = {"org.spring.learn"})
+@EnableScheduling
 public class App {
 
     public static void main(String...args){
--- spring-jpa-data-learn/src/main/java/org/springdata/learn/springjpadatalearn/SpringJpaDataLearnApplication.java
+++ spring-jpa-data-learn/src/main/java/org/springdata/learn/springjpadatalearn/SpringJpaDataLearnApplication.java
@@ -3,13 +3,16 @@ package org.springdata.learn.springjpadatalearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-
+@ComponentScan(basePackages = "org.springdata.learn.springjpadatalearn")
+@EnableScheduling
 public class SpringJpaDataLearnApplication {
 
 	public static void main(String[] args) {
